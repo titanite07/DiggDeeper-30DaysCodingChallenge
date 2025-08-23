c0f41ce8a7d6f7fd9b3ce84af3e1b07f60c6bdca
titanite07 <tarunbalajiv@gmail.com>
2025-08-23 16:11:32 +0530

    Day-1 Added

---
+++ Day-1/2235-Addtwointegers.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+class Solution {
+public:
+    int sum(int num1, int num2) {
+        ll res=(ll)num1+(ll)num2;
+        return (int)res;
+    }
+};
